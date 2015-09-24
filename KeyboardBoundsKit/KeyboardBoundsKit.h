aac21f4140127db41f101b7bf00ac9cf5a2a0303
驕比ｹ滓棊 <t_hayashi@hayashitatsuya-no-mac-pro.local>
2015-09-24 21:20:28 +0900

    Initial Commit

---
+++ KeyboardBoundsKit/KeyboardBoundsKit.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardBoundsKit.h
+//  KeyboardBoundsKit
+//
+//  Created by 林達也 on 2015/09/24.
+//  Copyright © 2015年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardBoundsKit.
+FOUNDATION_EXPORT double KeyboardBoundsKitVersionNumber;
+
+//! Project version string for KeyboardBoundsKit.
+FOUNDATION_EXPORT const unsigned char KeyboardBoundsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardBoundsKit/PublicHeader.h>
+
+
